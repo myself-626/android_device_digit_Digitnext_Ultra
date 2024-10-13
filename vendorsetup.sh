d6734d9407a3f904b0a620dfb4f7bec0658e06f1
myself <83259411+myself-626@users.noreply.github.com>
2024-10-13 19:08:26 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Digitnext_Ultra-user
-add_lunch_combo omni_Digitnext_Ultra-userdebug
-add_lunch_combo omni_Digitnext_Ultra-eng
+add_lunch_combo twrp_Digitnext_Ultra-user
+add_lunch_combo twrp_Digitnext_Ultra-userdebug
+add_lunch_combo twrp_Digitnext_Ultra-eng
